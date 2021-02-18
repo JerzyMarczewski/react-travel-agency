1b8b680a47e57bf6ba7d47be638ae22ea9921d53
JerzyMarczewski <jerzyk.marczewski@gmail.com>
2021-02-18 17:52:24 +0100

    Added jumbotron component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import mountainPhoto from "./images/mountain.jpg";
 import palmsPhoto from "./images/palms.jpg";
 import beachPhoto from "./images/beach.jpg";
 import MultiPhotoBlock from "./components/MultiPhotoBlock/MultiPhotoBlock";
+import ThreePhotoBlock from "./components/ThreePhotoBlock/ThreePhotoBlock";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
 
 const App = () => {
     return (
@@ -33,6 +35,30 @@ const App = () => {
             heading="What people think"
             text="We really liked it."
             text2=" - Bonnie and Clyde"
+            />
+            <ThreePhotoBlock 
+                heading="Special Deals and Last-Minute Offers"
+                description="Book a unique tour at a great price! Grab a last-minute 
+                    offer and start packing for your memorable vacation."
+                path1={beachPhoto}
+                heading1="Qatar Adventure"
+                text1="Deals at hotels with desert excursions included."
+                buttonText1="From $900"
+                path2={tajMahalPhoto}
+                heading2="Discover Greece"
+                text2="Visit Santorini and learn about its architecture and history."
+                buttonText2="From $650"
+                path3={palmsPhoto}
+                heading3="Explore Thailand"
+                text3="Indulge by the beach and relax with a massage."
+                buttonText3="From $1200"
+            />
+            <Jumbotron 
+                image={palmsPhoto}
+                heading1="Exclusive Offer"
+                heading2="Get 20% Off Your First Trip"
+                text="Who doesn’t like a discount? Book with us your first trip today and receive an exclusive offer."
+                buttonText="Explore Our Offers"
             />
 
         </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,10 +5,10 @@ import OffersButton from "../OffersButton/OffersButton";
 const Card = (props) => {
     return (
         <div className={styles.container}> 
-            <div className="image" style={{backgroundImage: `url(${props.path})`}}></div>
-            <h1>{props.heading}</h1>
-            <p>{props.text}</p>
-            <OffersButton text={props.buttonText}/>
+            <div className={styles.image} style={{backgroundImage: `url(${props.path})`}}></div>
+            <h1 className={styles.heading}>{props.heading}</h1>
+            <p className={styles.text}>{props.text}</p>
+            {props.buttonText ? <OffersButton text={props.buttonText}/> : ""}
         </div>
     )
 }
