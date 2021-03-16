ee8ce631f2e5959c8cf662da0259ac8c7e4b10bc
JerzyMarczewski <jerzyk.marczewski@gmail.com>
2021-03-17 00:39:01 +0100

    added hamburger animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
     BrowserRouter,
     Switch,
@@ -7,26 +6,28 @@ import {
 } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
-import Services from "./pages/Services.js";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Team from "./pages/Team";
-import News from "./pages/News";
-import Contact from "./pages/Contact";
+// import Services from "./pages/Services.js";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Team from "./pages/Team";
+// import News from "./pages/News";
+// import Contact from "./pages/Contact";
 
   const App = () => {
+
       return (
+          
         <BrowserRouter>
             <Navbar />
             
-            <Switch>
+            {/* <Switch>
                 <Route path="/services" render={Services}/>
                 <Route path="/about" render={About}/>
                 <Route path="/team" render={Team}/>
                 <Route path="/news" render={News}/>
                 <Route path="/contact" render={Contact}/>
                 <Route path="/" render={Home}/>
-            </Switch>
+            </Switch> */}
 
 
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import styles from "./Navbar.module.css";
+import "./Navbar.css";
 
 const Navbar = () => {
-
     
+    const [hamburgerClicked, setHamburgerClicked] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
+    window.addEventListener('resize', () => setWidth(window.innerWidth));
 
-    const [hiddenOptions, setHiddenOptions] = useState(true);
-
-    const handleMenuClick = () => {
-        setHiddenOptions(!hiddenOptions);
-    }
-
-    const handleResize = () => {
-        if (window.innerWidth >= 768){
-            setHiddenOptions(false);
-        } else {
-            setHiddenOptions(true);
-        }
-    }
-
-    useEffect(handleResize,[]);
-
-    window.addEventListener("resize", handleResize);
 
     return (
-        <div className={styles.container}>
-            <Link to="/"><h1 className={styles.name}>Viaggio</h1></Link>
-            <p className={styles.motto}>Vacations just for you</p>
-            <button className={styles.btn} onClick={handleMenuClick}>Menu{ hiddenOptions ? "+" : "-" }</button>
-            <nav className={hiddenOptions ? styles.options && styles.hidden : styles.options }>
-                <h6><Link to="/">Home</Link></h6>
-                <h6><Link to="/services">Services</Link></h6>
-                <h6><Link to="/about">About</Link></h6>
-                <h6><Link to="/team">Team</Link></h6>
-                <h6><Link to="/news">News</Link></h6>
-                <h6><Link to="/contact">Contact</Link></h6>
-            </nav>
-            <div className={styles.icons}>
-                <i class="fab fa-facebook-square" />
-                <i class="fab fa-twitter" />
-                <i class="fab fa-instagram" />
-                <i class="fab fa-snapchat-ghost" />
-                <i class="fab fa-tumblr"/>
+        <div className="navbar">
+            <div className="logo">{width}</div>
+            <div className={hamburgerClicked ? "hamburger clicked" : "hamburger"} onClick={() => setHamburgerClicked(!(hamburgerClicked))}>
+                <div className="line"></div>
             </div>
+            {/* <div className="options">
+                <Link>Home</Link>
+                <Link>Services</Link>
+                <Link>About</Link>
+                <Link>Team</Link>
+                <Link>News</Link>
+                <Link>Contact</Link>
+            </div> */}
         </div>
     )
 }
