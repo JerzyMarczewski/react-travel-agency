9e7c0bebfde93ff1c2051961232fdccb6b5de85b
JerzyMarczewski <jerzyk.marczewski@gmail.com>
2021-03-24 10:05:24 +0100

    completed block2 component

--- src/components/Block2/Block2.js
+++ src/components/Block2/Block2.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "./Block2.module.css";
-import woods from "../../images/woods.jpg";
+import italy from "../../images/Block2-italy.jpg";
+import jamaica from "../../images/Block2-jamaica.jpg";
+import kenya from "../../images/Block2-kenya.jpg";
+import mexico from "../../images/Block2-mexico.jpg";
 import PillButton from "../PillButton/PillButton";
 
 /* 
@@ -17,13 +20,35 @@ const Block2 = () => {
         <div className={styles.container}>
             <h1 className={styles.heading}>Upcoming Tours and Destinations</h1>
             <p className={styles.text}>From exotic places to ski resorts, the list is almost endless and we have an offer for you.</p>
-            <div className={styles.imageContainer}>
-                <div className={styles.imgBox}><img src={woods} alt="..." className={styles.img}/></div>
-                <div className={styles.imgBox}><img src={woods} alt="..." className={styles.img}/></div>
-                <div className={styles.imgBox}><img src={woods} alt="..." className={styles.img}/></div>
-                <div className={styles.imgBox}><img src={woods} alt="..." className={styles.img}/></div>
+            <div className={styles.imagesContainer}>
+                <div className={styles.imageContainer}>
+                    <div src={italy} alt="..." className={styles.image} style={{backgroundImage: `url(${italy})`}} />
+                    <div className={styles.overlay}>
+                        <p className={styles.imageText}>San Quirico d'Orcia, Italy</p>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <div src={kenya} alt="..." className={styles.image} style={{backgroundImage: `url(${kenya})`}} />
+                    <div className={styles.overlay}>
+                        <p className={styles.imageText}>Amboseli National Park, Kenya</p>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <div src={jamaica} alt="..." className={styles.image} style={{backgroundImage: `url(${jamaica})`}} />
+                    <div className={styles.overlay}>
+                        <p className={styles.imageText}>Ocho Rios, Jamaica</p>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <div src={mexico} alt="..." className={styles.image} style={{backgroundImage: `url(${mexico})`}} />
+                    <div className={styles.overlay}>
+                        <p className={styles.imageText}>San Miguel de Allende, Mexico</p>
+                    </div>
+                </div>
+                
+
             </div>
-            <PillButton value="More Offers" />
+            <PillButton value="Offers" />
         </div>
     )
 }
